3cb9436e4ed31d64c539e8a0b2f9b3ec58e459e4
PrasannaBalaji04 <prasannabalaji.rangarajan@bounteous.com>
2023-09-05 12:10:11 +0530

    change scopes for Stub and handled if conditions

--- src/files/__test__/mocha.test.ts
+++ src/files/__test__/mocha.test.ts
@@ -1,11 +1,18 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { fetchPostByUserId } from '../mocha';
 
 describe('fetchPostByUserId', () => {
+    let axiosStub: sinon.SinonStub;
+
+    beforeEach(() => {
+        axiosStub = sinon.stub(axios, 'get');
+    });
+    afterEach(() => {
+        axiosStub.restore();
+    });
     it('should fetch a post by user ID successfully', async () => {
-        const axiosStub = sinon.stub(axios, 'get');
         axiosStub.resolves({ status: 200, data: [{ userId: '1', id: '1', title: 'Test Post', body: 'This is a test post' }] });
 
         const userId = '1';
@@ -13,29 +20,22 @@ describe('fetchPostByUserId', () => {
 
         expect(result).to.deep.equal({ userId: '1', id: '1', title: 'Test Post', body: 'This is a test post' });
 
-        axiosStub.restore();
     });
 
     it('should handle an HTTP request error', async () => {
-        const axiosStub = sinon.stub(axios, 'get');
         axiosStub.rejects({ response: { status: 404 } });
 
-        const id = '1';
+        const id = 'a';
         const result = await fetchPostByUserId(id);
 
         expect(result).to.be.an('error');
         if (result instanceof Error) {
             expect(result.message).to.equal('Failed to fetch post for user with ID 1: Request failed with status code 404');
         }
-        else {
-            console.log(result);
-            expect.fail("Recieved data while expecting Error!")
-        }
-        axiosStub.restore();
+
     });
 
     it('should handle no posts found for the given user ID', async () => {
-        const axiosStub = sinon.stub(axios, 'get');
         axiosStub.resolves({ status: 200, data: [] });
 
         const id = 'a';
@@ -45,10 +45,6 @@ describe('fetchPostByUserId', () => {
         if (result instanceof Error) {
             expect(result.message).to.equal(`Failed to fetch post for user with ID ${id}: No posts found for the given ID`);
         }
-        else {
-            console.log(result);
-            expect.fail("Recieved data while expecting Error!");
-        }
-        axiosStub.restore();
+
     });
 });
